c0652b319bad07cf0bd39f2874482f9fe49c2110
petronio5 <petronio.55@hotmail.com>
2023-04-24 14:22:14 -0300

    cors

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const consign = require("consign");
+const cors = require("cors");
 
 module.exports = () => {
   const app = express();
@@ -8,6 +9,8 @@ module.exports = () => {
   app.set("port", process.env.PORT || config.get("server.port"));
 
   app.use(express.json());
+
+  app.use(cors());
 
   consign().then("data").then("controllers").then("routes").into(app);
 
